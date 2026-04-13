7b769ca13966aed564f36844252eff6ec960db43
kabir <kabirkakdiya@gmail.com>
2026-04-13 08:47:37 +0530

    refactor: used sql for vector similarity

--- src/database/crud_images.py
+++ src/database/crud_images.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Tuple
 from sqlalchemy.orm import Session
-from sqlalchemy import desc
+from sqlalchemy import desc, text
 from sqlalchemy.exc import IntegrityError
 from models.models import Image
 import os
@@ -34,23 +34,23 @@ def find_most_similar(
     query_embedding: List[float], 
     threshold: float = float(os.getenv("THRESHOLD", 0.80))
 ) -> Optional[Tuple[int, str, float]]:
-    similarity_expr = 1 - Image.embedding.cosine_distance(query_embedding)
     
-    similarity_percentage_expr = (1 + similarity_expr)/2 * 100 # to inflate the resulting percentage, 0.0 -> 50% and 1.0 -> 100%
-
-
-# SELECT image_id, (1 - (embedding1 <=> embedding2)) * 100 AS similarity_percentage FROM image_tbl WHERE (1- (embedding1 <=> embedding2)) > 0.85 ORDER BY similarity_percentage DESC LIMIT 1;
-    result = session.query(
-        Image.id,
-        Image.image_path,
-        similarity_percentage_expr.label('similarity_percentage')
-    ).filter(
-        similarity_expr > threshold  # WHERE (1 - distance) > 0.85
-    ).order_by(
-        desc('similarity_percentage')
-    ).first()
-
+    sql = text("""
+        SELECT 
+            id, 
+            image_path, 
+            ((1 + (1 - (embedding <=> :embedding))) / 2) AS similarity_percentage
+        FROM images
+        WHERE (1 - (embedding <=> :embedding)) > :threshold
+        ORDER BY similarity_percentage DESC
+        LIMIT 1
+    """)
+    result = session.execute(
+        sql, 
+        {"embedding": str(query_embedding), "threshold": threshold}
+    ).fetchone()
+    
     if not result:
         return None
 
-    return result.id, result.image_path, round(float(result.similarity_percentage), 2)
+    return result[0], result[1], round(float(result[2]), 2)
