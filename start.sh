8986a789e472538e1866040121f6cf63af972586
Kabir Kakdiya <kabirkakdiya@gmail.com>
2026-05-05 11:48:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ if [ ! -f .env ] && [ -f .env.example ]; then
   cp .env.example .env
 fi
 
-exec uv run --env-file=.env fastapi dev src/main.py --host 0.0.0.0 --port 7860
+exec uv run fastapi dev src/main.py --host 0.0.0.0 --port 7860
